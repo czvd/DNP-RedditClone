4ffeb8037bdd5e51580a63e90ab37367a55542ce
Carolina Valencia Zavidei <zavidei.carolina@gmail.com>
2025-10-12 16:46:34 +0200

    commit

---
+++ RepositoryContracts/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface ICommentRepository
+{
+    Task<Comment> AddAsync(Comment post);
+    Task UpdateAsync(Comment comment);
+    Task DeleteAsync(int id);
+    Task<Comment> GetSingleAsync(int id);
+    IQueryable<Comment> GetManyAsync();
+}
